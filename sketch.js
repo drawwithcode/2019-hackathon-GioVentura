6b00708ba39a81ccc4338cafa4b2142b5c923699
Giorgio Ventura <ventura.giorgio.95@gmail.com>
2019-11-27 12:30:32 +0100

    last

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ var offButton;
 var amp;
 var logo;
 
+var bubbles = [];
+
 
 function preload() {
   logo = loadImage("logo.png");
@@ -13,6 +15,7 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 
   sigla = loadSound("TG1_new.mp3", loaded);
   emilio = loadSound("cfdm.mp3", emiloaded);
@@ -27,27 +30,64 @@ function setup() {
 
   amp = new p5.Amplitude();
 
+
+  for (let i = 0; i < 300; i++) {
+    let x = random(width);
+    let y = random(height);
+    let r = random(7, 35);
+    bubbles[i] = new Bubble(x, y, r);
+  }
+
+
 }
 
 
 
 function draw() {
-  background(0);
+  background('#397091');
 
   var vol = amp.getLevel();
-  var diam = map(vol, 0, 0.7, 100, 1000);
+  var diam = map(vol, 0, 0.7, 150, 1400);
+  var bubmove = map(vol, 0, 0.7, 0, 40);
 
   imageMode(CENTER),
-  image(logo, width / 2, height / 2, diam, diam,);
-  sigla.setVolume(0.7);
-  emilio.setVolume(1);
+    image(logo, width / 2, height / 2, diam, diam, );
+  sigla.setVolume(0.8);
+  emilio.setVolume(3.5);
 
-}
-function loaded() {
-  sigla.play();
-  emilio.stop();
-}
-function emiloaded() {
-  emilio.play();
-  background(255);
-}
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].move(bubmove);
+    bubbles[i].show();
+  }
+  }
+
+  class Bubble {
+    constructor(_x, _y, _r) {
+      this.x = _x;
+      this.y = _y;
+      this.r = _r;
+    }
+    move(_more) {
+      this.x = this.x + random(-_more, _more);
+      this.y = this.y + random(-_more, _more);
+    }
+
+    show() {
+      stroke(255, 20);
+      strokeWeight(3.4);
+      fill(255, 30); // il secondo valore è l'alpha.
+      ellipse(this.x, this.y, this.r * 2);
+    }
+
+
+  }
+
+
+  function loaded() {
+    sigla.play();
+    emilio.stop();
+  }
+
+  function emiloaded() {
+    emilio.play();
+  }
